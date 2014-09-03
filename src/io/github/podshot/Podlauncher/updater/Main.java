b51978c5a291f0ff636be331c2ddb082f79e04b3
Podshot <ben.gothard3@gmail.com>
2014-09-02 21:06:41 -0400

    Added Canidate ability, will replace Runtime.exec ProcessBuilder soon

--- src/io/github/podshot/Podlauncher/updater/Main.java
+++ src/io/github/podshot/Podlauncher/updater/Main.java
@@ -12,12 +12,24 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JProgressBar;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		boolean canidateUpdate = false;
+		for (String arg : args) {
+			if (arg.equals("-canidate")) {
+				canidateUpdate = true;
+			}
+		}
 
-		String newPodLauncherFile = "http://podshot.github.io/PodLauncher/update/PodLauncher.jar";
+		String newPodLauncherFile = "https://dl.dropbox.com/s/xr0ksjzutaapx7g/Test-PodLauncher.jar";
+		if (canidateUpdate) {
+			newPodLauncherFile = "http://podshot.github.io/PodLauncher/update/canidate.jar";
+		}
 		String filename = "PodLauncher.jar";
 
 		JFrame frame = new JFrame();
@@ -34,6 +46,14 @@ public class Main {
 		lblUpdatingPodlauncher.setHorizontalAlignment(SwingConstants.CENTER);
 		lblUpdatingPodlauncher.setBounds(119, 50, 146, 14);
 		frame.getContentPane().add(lblUpdatingPodlauncher);
+		
+		JLabel lblCanidateUpdate = new JLabel("Canidate Update");
+		lblCanidateUpdate.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCanidateUpdate.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblCanidateUpdate.setBounds(119, 103, 136, 14);
+		lblCanidateUpdate.setVisible(canidateUpdate);
+		
+		frame.getContentPane().add(lblCanidateUpdate);
 		frame.setVisible(true);
 		frame.setSize(400, 200);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
